c83aa8ddf94054972e9c2c72aa751c249d0497e4
Sophia Barrett <sophia.barrett@gmail.com>
2018-10-04 16:14:46 -0400

    third commit adding graphs and the Rscript for the descriptive statistics

--- salaries.R
+++ salaries.R
@@ -12,7 +12,19 @@ T <- table(salaries$rank, salaries$sex)
 addmargins(T, c(1, 2))
 
 
-spineplot(T, col = c('pink', 'blue'))
+spineplot(T, )
 
 barplot(T, col = c('pink', 'blue', 'green'), beside = TRUE, legend.text = TRUE)
+str(salaries)
+hist(salaries$salary)
+hist(salaries$salary, main = "Overall Professor Salary", xlab = "Salary")
+hist(salaries$yrs.since.phd, main = "Overall Years Since Ph.D.", xlab = "years since Ph.D.")
+hist(salaries$yrs.service, main = "Overall of Service", xlab = "years of service")
 
+mean(salaries$salary)
+mean(salaries$yrs.service)
+mean(salaries$yrs.since.phd)
+
+sd(salaries$salary)
+sd(salaries$yrs.service)
+sd(salaries$yrs.since.phd)
